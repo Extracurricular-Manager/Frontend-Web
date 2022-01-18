d12831ce903e4c583b37d0d8314f17c0f31b647a
GuillaumeSUBRA <guillaumesubra1@gmail.com>
2022-01-18 16:00:58 +0100

    update

--- front-end-web/src/app/children/classes-list/classes-list.component.ts
+++ front-end-web/src/app/children/classes-list/classes-list.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClassesListComponent implements OnInit {
 
+  classes:string[] = ["CP","CE1-CE2","CM1-CM2"];
   constructor() { }
 
   ngOnInit(): void {
