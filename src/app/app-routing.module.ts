df755799cf31cd5e20024a26cc9541334666d606
Guillaume <51230568+tartiflm@users.noreply.github.com>
2022-02-21 15:55:01 +0100

    began work for navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ChildrenComponent} from "./children/children.component";
+import {BillingViewComponent} from "./billing-view/billing-view.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'bonjour', component:BillingViewComponent},
+  {path: 'home', component:ChildrenComponent},
+  {path:'', redirectTo:'home',pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { SettingsViewComponent } from './settings-view/settings-view.component';
 import { ModulesViewComponent } from './modules-view/modules-view.component';
 import { ClassChildrenViewComponent } from './class-children-view/class-children-view.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -32,7 +34,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
