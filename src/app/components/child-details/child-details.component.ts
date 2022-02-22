efc974c1c70325cde7d3f6f62596ceb70e6ce6b3
Guillaume <51230568+tartiflm@users.noreply.github.com>
2022-02-22 15:29:10 +0100

    refactor + began children view

---
+++ src/app/components/child-details/child-details.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child-details',
+  templateUrl: './child-details.component.html',
+  styleUrls: ['./child-details.component.scss']
+})
+export class ChildDetailsComponent implements OnInit {
+  child = {
+    "name":"",
+    "surname":"",
+    "classroom":{
+      "name":""
+    },
+    diet:{
+      "name":""
+    }
+  };
+  weekAmount: any;
+  monthAmount: any;
+  totalAmount: any;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
