b065a79b4fd064c886535a56b60235c64fe00847
Guewen <guewen.morice@gmail.com>
2022-03-03 09:27:15 +0100

    cleaning a bit

--- src/app/view/class-children-view/class-children-view.component.ts
+++ src/app/view/class-children-view/class-children-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Child} from "../../../api/data/child";
+import { Adelphie } from 'src/api/data/adelphie';
+import { Child } from "../../../api/data/child";
 
 @Component({
   selector: 'app-class-children-view',
@@ -8,6 +9,7 @@ import {Child} from "../../../api/data/child";
 })
 export class ClassChildrenViewComponent implements OnInit {
 
+  fam: Adelphie | undefined;
   val: Child | undefined;
 
   constructor() { }
@@ -15,7 +17,7 @@ export class ClassChildrenViewComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  setName(val:any){
+  setName(val: any) {
     this.val = val
     console.log(val)
   }
